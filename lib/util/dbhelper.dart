801417dfe1f428d859f697b9ad757de644dd697a
Abdullah Al Asad <info.alasad@gmail.com>
2019-01-13 19:33:11 +0600

    dbHelper ongoing

--- lib/util/dbhelper.dart
+++ lib/util/dbhelper.dart
@@ -19,10 +19,28 @@ class DbHelper {
     return _dbHelper;
   }
 
+  static Database _db;
+
+  Future<Database> get db async {
+    if(_db == null) {
+      _db = await initializeDb();
+    }
+    return _db;
+  }
+
   Future<Database> initializeDb() async {
     Directory dir = await getApplicationDocumentsDirectory();
     String path = dir.path + "todos.db";
-    
+    var dbTodos = await openDatabase(path,version: 1,onCreate: _createDb)
+    return dbTodos;
+
+  }
+
+  void _createDb(Database db, int newVersion) async {
+    await db.execute(
+      "CREATE TABLE $tblToDo($colId INTEGER PRIMARY KEY, $colTitle TEXT, " +
+      "$colDescription TEXT, $colPriority INTEGER, $colDate TEXT"
+    );
   }
 
 }
