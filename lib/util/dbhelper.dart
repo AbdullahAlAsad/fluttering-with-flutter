25a3df0b8c7ac385ef822cc11ffaeeb536ae36ca
Abdullah Al Asad <info.alasad@gmail.com>
2019-01-13 18:48:52 +0600

    todo app using flutter

---
+++ lib/util/dbhelper.dart
@@ -0,0 +1,28 @@
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:todo_app/model/todo.dart';
+
+class DbHelper {
+  static final DbHelper _dbHelper = new DbHelper._internal();
+  String tblToDo = "todo";
+  String colId = "id";
+  String colTitle = "title";
+  String colDescription = "description";
+  String colPriority = "priority";
+  String colDate = "date";
+
+  DbHelper._internal();
+
+  factory DbHelper() {
+    return _dbHelper;
+  }
+
+  Future<Database> initializeDb() async {
+    Directory dir = await getApplicationDocumentsDirectory();
+    String path = dir.path + "todos.db";
+    
+  }
+
+}
