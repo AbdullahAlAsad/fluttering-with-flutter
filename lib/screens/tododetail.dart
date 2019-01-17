59bd3a4a026ec0a3bcf170d1c530b5877e368159
Abdullah Al Asad <info.alasad@gmail.com>
2019-01-17 17:51:08 +0600

    tododetail with action menu

--- lib/screens/tododetail.dart
+++ lib/screens/tododetail.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/model/todo.dart';
 import 'package:todo_app/util/dbhelper.dart';
+import 'dart:async';
 import 'package:intl/intl.dart';
+
+DbHelper helper = DbHelper();
+final List<String> choices = const <String> [
+  'Save Todo & Back',
+  'Delete Todo',
+  'Back to List'
+];
+
+const mnuSave = 'Save Todo & Back';
+const mnuDelete = 'Delete Todo';
+const mnuBack = 'Back to List';
 
 class TodoDetail extends StatefulWidget {
   final Todo todo;
@@ -29,14 +41,37 @@ class TodoDetailState extends State {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: Text(todo.title),
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: select,
+            itemBuilder: (BuildContext context){
+              return choices.map((String choice){
+                  return PopupMenuItem(
+                    value: choice,
+                    child:  new Container(
+                          child: new Card(
+                            child: new Row(children: <Widget>[
+                              new Icon(Icons.menu),
+                              new Text(choice),
+                            ]),
+                          ),
+                          // color: Colors.deepOrangeAccent,
+                          alignment: Alignment.center,//Alignment(0.0, 0.0),
+                        ),
+                  );
+              }).toList();
+            },
+          ),
+        ],
       ),
       body: Padding(
         padding: EdgeInsets.only(
           left: 10.0,
           right: 10.0,
         ),
-        child: SingleChildScrollView(
-          child: Column(
+        child: ListView(
+          children: <Widget>[ 
+            Column(
             children: <Widget>[
               Padding(
                 padding: EdgeInsets.only(
@@ -68,38 +103,84 @@ class TodoDetailState extends State {
                           borderRadius: BorderRadius.circular(5.0))),
                 ),
               ),
-              DropdownButton<String>(
-                  style: Theme.of(context).textTheme.title,
-
-                  // isExpanded: true,
-                  items: _priorities.map((String val) {
-                    return new DropdownMenuItem<String>(
-                      value: val,
-                      child: new Container(
-                        child: new Card(
-                          child: new Row(children: <Widget>[
-                            new Icon(Icons.low_priority),
-                            new Text(val),
-                          ]),
+              ListTile(
+                              title: DropdownButton<String>(
+                    style: textStyle,
+                    // isExpanded: true,
+                    items: _priorities.map((String val) {
+                      return new DropdownMenuItem<String>(
+                        value: val,
+                        child: new Container(
+                          child: new Card(
+                            child: new Row(children: <Widget>[
+                              new Icon(Icons.low_priority),
+                              new Text(val),
+                            ]),
+                          ),
+                          // color: Colors.deepOrangeAccent,
+                          alignment: Alignment.center,//Alignment(0.0, 0.0),
                         ),
-                        color: Colors.deepOrangeAccent,
-                      ),
-                    );
-                  }).toList(),
-                  hint: new Card(
-                      child: new Row(children: <Widget>[
-                    new Icon(Icons.person),
-                    new Text("check"),
-                  ])),
-                  value: _priority,
-                  onChanged: (newVal) {
-                    _priority = newVal;
-                    setState(() {});
-                  })
+                      );
+                    }).toList(),
+                    hint: new Card(
+                        child: new Row(children: <Widget>[
+                      new Icon(Icons.low_priority),
+                      new Text("check"),
+                    ])),
+                    value: _priority,
+                    onChanged: (newVal) {
+                      _priority = newVal;
+                      setState(() {});
+                    }),
+              )
             ],
           ),
+          ],
         ),
       ),
     );
+  }
+
+  Future select (String value) async {
+    int result;
+    switch (value) {
+      case mnuSave:
+        save();
+        break;
+      case mnuDelete:
+      Navigator.pop(context,true);
+        if (todo.id == null) {
+          return;
+        }
+        result = await helper.deleteTodo(todo.id);
+        if (result != 0) {
+          AlertDialog alertDialog = AlertDialog(
+            title: Text("Delete Todo"),
+            content: Text("The Todo has been deleted"),
+          );
+          showDialog(
+            context: context,
+            builder: (_) => alertDialog
+          );
+        }
+        break;
+       case mnuBack:
+        Navigator.pop(context,true);
+        //save();
+        break;       
+      default:
+    }
+  }
+
+  void save() {
+    todo.date = new DateFormat.yMd().format(DateTime.now());
+    if(todo.id !=null) {
+      helper.updateTodo(todo);
+    }
+    else {
+      helper.insertTodo(todo);
+    }
+    Navigator.pop(context,true);
+
   }
 }
